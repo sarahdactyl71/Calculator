7fac5ef211567b9ec36f4558698d521b95f687ad
Sarah Kirk <kirk710@gmail.com>
2017-03-20 19:29:19 -0600

    adds the files

---
+++ calculator.rb
@@ -0,0 +1,5 @@
+#Calculator Homework
+
+class Calculator
+
+end
---
+++ calculator_test.rb
@@ -0,0 +1,15 @@
+gem 'minitest', '~> 5.0'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative 'calculator_homework'
+
+class CalculatorTest < Minitest::Test
+    def test_can_it_add
+    end
+    def test_can_it_subtract
+    end
+    def test_can_it_clear
+    end
+    def test_can_it_total
+    end
+end
